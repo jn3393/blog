bd4a509d78bfc9caa06abbf6eef1eea65f9dd6e5
jn3393 <87933360@qq.com>
2016-03-30 14:12:37 +0800

    1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Leatry Blog</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../resources/css/bootstrap.min.css" >
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- This is Navbar-->
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Leatry Blog</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home(首页) <span class="sr-only">(current)</span></a></li>
+                <li><a href="/about">About(关于)</a></li>
+                <li><a href="/contact">Contact(联系我们)</a></li>
+                <!--<li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>-->
+              </ul>
+              <!--<form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+              </form>-->
+              <ul class="nav navbar-nav navbar-right">
+              
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="jumbotron">
+                      <h1>Hello, world!</h1>
+                      <p class="lead">...</p>
+                      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+                    </div>                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="post">
+                        <h3>Post Tittle</h3>
+                        <p>范德萨范德萨范德萨范德萨分打算范德萨法大师傅大势发大水法大师傅大师傅的撒范德萨发大水发大水发范德萨范德萨发大水范德萨发大水啊阿三范德萨范德萨发大水法大师傅阿凡达是都是是是是地方撒范德萨范德萨地方撒深度j</p>
+                        <a href="" class="btn btn-primary ">More</a>
+                    </div>
+                    <hr>
+                    <div class="post">
+                        <h3>Post Tittle</h3>
+                        <p>jkfdls;ajfkldjsaljfldjslajfldjlsajfldjsklfjdlsj</p>
+                        <a href="" class="btn btn-primary ">More</a>
+                    </div>
+                    <hr>
+                    <div class="post">
+                        <h3>Post Tittle</h3>
+                        <p>jkfdls;ajfkldjsaljfldjslajfldjlsajfldjsklfjdlsj</p>
+                        <a href="" class="btn btn-primary ">More</a>
+                    </div>
+                    <hr>
+                    <div class="post">
+                        <h3>Post Tittle</h3>
+                        <p>jkfdls;ajfkldjsaljfldjslajfldjlsajfldjsklfjdlsj</p>
+                        <a href="" class="btn btn-primary ">More</a>
+                    </div>
+                    <hr>
+                </div>
+
+                <div class="col-md-3 col-md-offset-1">
+                    <h2>Sidebar</h2>
+                </div>
+            </div>
+            
+        </div>
+
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../resources/js/jquery-2.2.2.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../resources/js/bootstrap.min.js"></script>
+  </body>
+</html>
